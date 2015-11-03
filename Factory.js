583581a939d4474416d371b2b8c0dd658c0a10fa
terrence.tang <tay81084@gmail.com>
2015-11-03 17:29:01 +0800

    remove Singleton.html

--- Factory.js
+++ Factory.js
@@ -1,3 +1,41 @@
 /**
  * Created by tay81084 on 15/11/3.
  */
+
+var CarMaker = function(){};
+
+CarMaker.prototype.dirve = function(){
+    return "I have" + this.door + "doors";
+};
+
+CarMaker.factory = function(type){
+    var cr = type,
+        newcar;
+
+    if(typeof CarMaker[cr] !== "function"){
+        throw{
+            name:"Error",
+            Message: cr + "does not exist"
+        };
+    }
+
+    if(typeof CarMaker[cr].prototype.drive !== "function"){
+        CarMaker[cr].prototype = new CarMaker();
+    }
+
+    newcar = new CarMaker[cr]();
+    return newcar;
+}
+
+CarMaker.Compact = function(){
+    this.doors = 4;
+}
+
+CarMaker.Convertible = function(){
+    this.doors = 2;
+}
+
+CarMaker.SUV = function(){
+    this.doors = 24;
+}
+module.exports = CarMaker;
